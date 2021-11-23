c6f95d446104579646e49bc4e24c8d79562d5632
mkiselev2002 <mkiselev2002@icloud.com>
2021-11-23 11:27:36 +0000

    If youre the higest bidder you can no longer bid

--- place_bid.php
+++ place_bid.php
@@ -6,6 +6,26 @@ $bidValue = $_POST['bid']; # All bid value verification done via .js
 $buyerEmail = $_SESSION['username'];
 $auctionID = $_GET['item_id'];
 
+$query = "SELECT buyerEmail, MAX(bidValue)
+from bids
+WHERE auctionID =".$auctionID."
+GROUP BY buyerEmail";
+$initialResult = query($query);
+$number_of_bids = mysqli_num_rows($initialResult);
+if ($number_of_bids > 0){
+    $initialResult =  $initialResult -> fetch_assoc();
+    $email = $initialResult["buyerEmail"];
+}else{
+    $email = "Wrong Email";
+}
+
+if ($email == $buyerEmail){
+echo('<div class="text-center">You are already the highest bidder! Your bid has not been registered and you will be redirected shorty.</div>');
+header('refresh:5;url="listing.php?item_id='.$auctionID.'"');
+}else{
+
+
+
 # Generate a bid ID by querying the database
 $result = query("SELECT bidID FROM bids");
 $numrows = mysqli_num_rows($result);
@@ -22,6 +42,6 @@ query($query);
 
 # Notify and redirect
 echo('<div class="text-center">Bid placed successfully! You will be redirected shortly</div>');
-header('refresh:5;url="http://auction/listing.php?item_id='.$auctionID.'"');
-
+header('refresh:5;url="listing.php?item_id='.$auctionID.'"');
+}
 ?>
