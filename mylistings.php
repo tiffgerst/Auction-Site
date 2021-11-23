e8882ef33e7d057d439782a14e59c550d6f2d6c3
tiffgerst <tiffany.gerstmeyr@web.de>
2021-11-23 15:15:19 +0000

    readded mylisting

--- mylistings.php
+++ mylistings.php
@@ -6,6 +6,43 @@
 <h2 class="my-3">My listings</h2>
 
 <?php
+$email = $_SESSION["username"];
+$query = "SELECT a.auctionID, a.title, a.description, a.endDate, a.startPrice, ";
+  $query .= "COALESCE(b.current_price,startPrice) AS 'current_price', COALESCE(b.num_bids,0) AS 'num_bids' ";
+  $query .= "FROM ";
+  $query .= "(SELECT auctionID, MAX(bidValue) AS 'current_price', COUNT(auctionID) AS 'num_bids' FROM bids GROUP BY auctionID) b ";
+  $query .= "RIGHT JOIN (SELECT * FROM auctions a WHERE a.sellerEmail = '$email' ) a ";
+  $query .= "ON a.auctionID = b.auctionID ORDER BY current_price";
+
+  $result = query($query);
+  if($result){
+    $num_result = mysqli_num_rows($result);
+  }
+  else{
+    $num_result = 0;
+  }
+  if ($num_result>0) {
+    while ($row = $result->fetch_assoc()) {
+      $item_id = $row['auctionID'];
+      $title = $row['title'];
+      $description = $row['description'];
+      $end_date = new DateTime($row['endDate']); # Convert from string to DT object
+      $num_bids = $row['num_bids'];
+      
+      if ($num_bids == 0) {
+        $current_price = $row['startPrice'];
+      }
+      else {
+        $current_price = $row['current_price'];
+      }
+        
+      print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+    }
+  }
+  else if ($num_result==0){
+    # Result is empty
+    echo('No results found');
+  }
   // This page is for showing a user the auction listings they've made.
   // It will be pretty similar to browse.php, except there is no search bar.
   // This can be started after browse.php is working with a database.
