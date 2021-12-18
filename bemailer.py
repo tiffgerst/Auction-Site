b32690143649a0931a5a92eecbef38260290d1ee
Leo <zchaatu@ucl.ac.uk>
2021-12-18 15:29:13 +0000

    email shizzle

---
+++ bemailer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import sys 
+import bestemail as be
+
+if sys.argv[1] == '1':
+    be.main(sys.argv[1])
--- bestemail.py
+++ bestemail.py
@@ -4,42 +4,40 @@ import smtplib, ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
-import sys
+
+def main(b):
+    #database connection
+    sender = 'sillygoosecoursework@gmail.com'
+    password = '&C6QLPbNS&6iBdRQ'
+    port = 587    
+    smtp_server = "smtp.gmail.com"
+
+    connection = pymysql.connect(host="localhost",user="root",passwd="",database="auction")
+    cursor = connection.cursor()
+    query = f"SELECT `buyerEmail` FROM `bids` WHERE `auctionID`={b}"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    
+    
+    
+    result = (('tiffgerst@gmail.com'), ('leonardpaturel@gmail.com'))
+    for row in result:
+        print(row)
+        receiver = f'{row}'
+        message = f"""\
+    Subject: Hi there
+
+    This message is sent from Python. you won!"""
+    
+        context = ssl.create_default_context()
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls(context=context)
+            server.ehlo()
+            server.login(sender, password)
+            server.ehlo()
+            server.sendmail(sender, receiver, message)
 
 
-#database connection
-connection = pymysql.connect(host="localhost",user="root",passwd="",database="auction", cursorclass=pymysql.cursors.DictCursor)
-cursor = connection.cursor()
-query = "INSERT INTO `users` (`email`, `password`, `accountType`, `country`, `addressLine`, `city`, `postcode`) VALUES('ben100@thread.com', '$2y$10$aR0n47yMHpE.H9b7MLUC4.UeTbX8CXm2tpQ8BqxjfPU.5XiIzgcui', 'buyer', 'Jamaica', '34 Richest Street', 'Richest City', 'RI3H M3')"
-cursor.execute(query)
-connection.commit()
-connection.close()
-
-
-
-
-
-# sender = 'sillygoosecoursework@gmail.com'
-# password = '&C6QLPbNS&6iBdRQ'
-
-
-# receiver = 'leonardpaturel@gmail.com'
-
-
-# port = 587    
-# smtp_server = "smtp.gmail.com"
-
-# message = f"""\
-# Subject: Hi there
-
-# This message is sent from Python. {sys.argv[1]}"""
-
-# context = ssl.create_default_context()
-# with smtplib.SMTP(smtp_server, port) as server:
-#     server.starttls(context=context)
-#     server.ehlo()
-#     server.login(sender, password)
-#     server.ehlo()
-#     server.sendmail(sender, receiver, message)
-
-
+if __name__ == '__main__':
+    main(1)
