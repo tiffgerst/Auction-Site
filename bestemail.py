9b02ae3c80d25876bab5ebbc02f3bb0c0a86a35a
Leo <zchaatu@ucl.ac.uk>
2021-12-18 12:27:26 +0000

    sql in python

--- bestemail.py
+++ bestemail.py
@@ -1,6 +1,5 @@
-#!C:\Users\leona\AppData\Local\Programs\Python\Python310\python.exe 
-
-
+import pymysql
+import pymysql.cursors
 import smtplib, ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
@@ -8,27 +7,39 @@ from email.mime.text import MIMEText
 import sys
 
 
-sender = 'sillygoosecoursework@gmail.com'
-password = '&C6QLPbNS&6iBdRQ'
+#database connection
+connection = pymysql.connect(host="localhost",user="root",passwd="",database="auction", cursorclass=pymysql.cursors.DictCursor)
+cursor = connection.cursor()
+query = "INSERT INTO `users` (`email`, `password`, `accountType`, `country`, `addressLine`, `city`, `postcode`) VALUES('ben100@thread.com', '$2y$10$aR0n47yMHpE.H9b7MLUC4.UeTbX8CXm2tpQ8BqxjfPU.5XiIzgcui', 'buyer', 'Jamaica', '34 Richest Street', 'Richest City', 'RI3H M3')"
+cursor.execute(query)
+connection.commit()
+connection.close()
 
 
-receiver = 'leonardpaturel@gmail.com'
 
 
-port = 587    
-smtp_server = "smtp.gmail.com"
 
-message = f"""\
-Subject: Hi there
+# sender = 'sillygoosecoursework@gmail.com'
+# password = '&C6QLPbNS&6iBdRQ'
 
-This message is sent from Python. {sys.argv[1]}"""
 
-context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.starttls(context=context)
-    server.ehlo()
-    server.login(sender, password)
-    server.ehlo()
-    server.sendmail(sender, receiver, message)
+# receiver = 'leonardpaturel@gmail.com'
+
+
+# port = 587    
+# smtp_server = "smtp.gmail.com"
+
+# message = f"""\
+# Subject: Hi there
+
+# This message is sent from Python. {sys.argv[1]}"""
+
+# context = ssl.create_default_context()
+# with smtplib.SMTP(smtp_server, port) as server:
+#     server.starttls(context=context)
+#     server.ehlo()
+#     server.login(sender, password)
+#     server.ehlo()
+#     server.sendmail(sender, receiver, message)
 
 
