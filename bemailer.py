42f067812ee8c1b2014bd371a2cc7af800a3b68a
Leo <zchaatu@ucl.ac.uk>
2021-12-18 22:25:01 +0000

    merge

--- bemailer.py
+++ bemailer.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 #!C:\Users\Leo\AppData\Local\Programs\Python\Python310\python.exe 
+=======
+#!/usr/bin/env python
+>>>>>>> 4b33d4a (email shizzle)
 
 import sys 
 import bestemail as be
